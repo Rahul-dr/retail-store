5ff93164a8b560cddecd80c0236d9b4772389fad
rahul224422 <agrivision177@gmail.com>
2025-03-02 19:24:16 +0530

    Updated code and fixed issues

--- src/main/java/com/project/retail_store/service/interfaces/AuthService.java
+++ src/main/java/com/project/retail_store/service/interfaces/AuthService.java
@@ -3,8 +3,12 @@ package com.project.retail_store.service.interfaces;
 import com.project.retail_store.dtos.AuthRequest;
 import com.project.retail_store.dtos.AuthResponse;
 import com.project.retail_store.dtos.RegisterRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface AuthService {
+
+    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
 
     AuthResponse register(RegisterRequest request);
 
